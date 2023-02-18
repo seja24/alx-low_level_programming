889f59e8eb91e7728056523010aaff2dd855d790
Seja <sejaedwina@gmail.com>
2023-02-18 10:04:22 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
  * main - A program that prints a line with puts function
- * Return: 0 (sucess)
+ *
+ *  Return: 0 (Success)
  */
 int main(void)
 {
 puts("\"programming is like building a multilingual puzzle");
+
 return (0);
 }
